0f37717412d1bebf409e8d232f2f3f4fd97f6543
luzhiqiang8081 <50010251+luzhiqiang8081@users.noreply.github.com>
2024-01-11 09:03:06 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const server = http.createServer((request, response) => {
+    const data = {
+        id: 1,
+        title: '关山月',
+        content: '明月几时有'
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    response.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    response.write(jsonData);
+
+    response.end();
+});
+
+server.listen(3000, () => {
+    console.log('服务启动!!!');
+});
